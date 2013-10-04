19c07c6b805974c0cc711727030af3aa029e0310
Justin Henry <doc2@fullsail.edu>
2013-10-04 01:53:51 -0500

    Finished Slice of the Pie Part I

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -9,7 +9,14 @@ dogAge = humanAge * 7; //definition and formula for calculating dog years based 
 console.log("Sparky is " + humanAge + " human years old, which is " + dogAge + " in dog years."); //Final product
 
 //Slice of Pie Part I
+var slices = 8; //declaration and definition
+var people = 8; //declaration and definition
+var pizzas = 4; //declaration and definition
+var slicesPer; //declaration
 
+slicesPer = (pizzas * slices) / people; //definition and formula for calculating how many slices each person gets
+
+console.log("Each person ate " + slicesPer + " slices of pizza at the party."); //Final quotient
 
 //Slice of Pie Part II
 
