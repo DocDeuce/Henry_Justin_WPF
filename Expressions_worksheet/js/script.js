a3e02ff9df4bdd3f2cbacb85b0c980be657eba0c
Justin Henry <doc2@fullsail.edu>
2013-10-04 01:36:21 -0500

    Completed Dog Years

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,2 +1,20 @@
 // Justin Henry 10-4-2013 Expressions Worksheet
-alert("This is in the js file");
+
+//Dog Years
+var humanAge = 1;
+var dogAge;
+
+dogAge = humanAge * 7;
+
+console.log("Sparky is " + humanAge + " human years old, which is " + dogAge + " in dog years.");
+
+//Slice of Pie Part I
+
+
+//Slice of Pie Part II
+
+
+//Average Shopping Bill
+
+
+//Discounts
