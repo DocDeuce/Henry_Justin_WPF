82d0f2f2bcc1dca12289d1bfab8877a8574b6a5b
Justin Henry <doc2@fullsail.edu>
2013-10-04 01:43:18 -0500

    Added comments to Dog Years

--- Expressions_worksheet/js/script.js
+++ Expressions_worksheet/js/script.js
@@ -1,12 +1,12 @@
 // Justin Henry 10-4-2013 Expressions Worksheet
 
 //Dog Years
-var humanAge = 1;
-var dogAge;
+var humanAge = 1; //declaration and definition
+var dogAge; //declaration
 
-dogAge = humanAge * 7;
+dogAge = humanAge * 7; //definition and formula for calculating dog years based on given human years
 
-console.log("Sparky is " + humanAge + " human years old, which is " + dogAge + " in dog years.");
+console.log("Sparky is " + humanAge + " human years old, which is " + dogAge + " in dog years."); //Final product
 
 //Slice of Pie Part I
 
