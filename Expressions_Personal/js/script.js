1ecc1427dd6b5f5703d9df4e4820ced62334e1a6
Justin Henry <doc2@fullsail.edu>
2013-10-05 17:22:08 -0500

    Added formulas to Expressions Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,9 @@ var milesPerGallon = prompt("Your vehicle's fuel economy in miles per gallon is"
 var milesToWork = prompt("In miles, how far is work from home?", "");
 var daysOfWork = prompt("How may days of the week do you work?", "");
 var gasPrice = prompt("What is the current price of gas in your area? ", "");
+
+var milesPerDay = milesToWork * 2;
+var milesPerWeek = milesPerDay * daysOfWork;
+var gallonsPerWeek = milesPerGallon / milesPerWeek;
+var gasBudget = gallonsPerWeek * gasPrice;
+
