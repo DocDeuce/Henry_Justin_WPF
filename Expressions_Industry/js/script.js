c1b43e55cb5f1d1bbce7064a7181fe0a5727b407
Justin Henry <doc2@fullsail.edu>
2013-10-05 16:34:54 -0500

    Commented on Expressions Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,27 +1,28 @@
 //Calculating student grade average with average of Tests, Quizzes, and Assignments having different weight
-var testWeight = 50;
-var quizWeight = 30;
-var assignWeight = 20;
+var testWeight = 50; //Declaration and definition of the percentage of weight for tests
+var quizWeight = 30; //Declaration and definition of the percentage of weight for quizzes
+var assignWeight = 20; //Declaration and definition of the percentage of weight for assignments
 
-var assignGrade1 = prompt("Enter student's first assignment grade", "");
-var assignGrade2 = prompt("Enter student's second assignment grade", "");
-var quizGrade = prompt("Enter student's quiz grade", "");
-var testGrade = prompt("Enter student's test grade", "");
+var assignGrade1 = prompt("Enter student's first assignment grade", ""); //Prompt for user to enter a value for student's first assignment grade
+var assignGrade2 = prompt("Enter student's second assignment grade", ""); //Prompt for user to enter a value for student's second assignment grade
+var quizGrade = prompt("Enter student's quiz grade", ""); //Prompt for user to enter a value for student's quiz grade
+var testGrade = prompt("Enter student's test grade", ""); //Prompt for user to enter a value for student's test grade
 
-var assignArray = [assignGrade1, assignGrade2];
-var assignAvg = (parseInt(assignArray[0]) + parseInt(assignArray[1]))/2;
+var assignArray = [assignGrade1, assignGrade2]; //Declaration and definition of array containing assignment grades
+var assignAvg = (parseInt(assignArray[0]) + parseInt(assignArray[1]))/2; //Declaration, definition, and formula for calculating the average of student's assignment grades
 
-var assignFinal = assignWeight * (assignAvg/100);
-var quizFinal = quizWeight * (quizGrade/100);
-var testFinal = testWeight * (testGrade/100);
-var finalAvg = assignFinal + quizFinal + testFinal;
+var assignFinal = assignWeight * (assignAvg/100); //Declaration, definition, and formula for calculating the assignment grade with weight
+var quizFinal = quizWeight * (quizGrade/100); //Declaration, definition, and formula for calculating the quiz grade with weight
+var testFinal = testWeight * (testGrade/100); //Declaration, definition, and formula for calculating the test grade with weight
+var finalAvg = assignFinal + quizFinal + testFinal; //Declaration, definition, and formula for calculating student's grade
 
 document.write("Assignments have a weight of " + assignWeight + "%, quizzes have a weight of " + quizWeight + "%, and tests have a weight of " + testWeight + "%. Based on this and the grades you entered, your student's total class grade is " + finalAvg + "." + "<BR>" + "<BR>");
+//Summary of information entered and calculated
 
-var gradeArray = [assignArray, quizGrade, testGrade, finalAvg];
+var gradeArray = [assignArray, quizGrade, testGrade, finalAvg]; //Declaration and definition of array containing all grades
 
-document.write("Assignment Grade 1: " + assignArray[0] + "<BR>");
-document.write("Assignment Grade 2: " + assignArray[0] + "<BR>");
-document.write("Quiz Grade : " + gradeArray[1] + "<BR>");
-document.write("Test Grade : " + gradeArray[2] + "<BR>");
-document.write("Class Grade : " + gradeArray[3] + "<BR>");
+document.write("Assignment Grade 1: " + assignArray[0] + "<BR>"); //Display of assignment grade 1
+document.write("Assignment Grade 2: " + assignArray[0] + "<BR>"); //Display of assignment grade 2
+document.write("Quiz Grade : " + gradeArray[1] + "<BR>"); //Display of quiz grade
+document.write("Test Grade : " + gradeArray[2] + "<BR>"); //Display of test grade
+document.write("Class Grade : " + gradeArray[3] + "<BR>"); //Display of class grade
