9c1a5ca90361f5c82063de972d5c1ecb080524c9
Justin Henry <doc2@fullsail.edu>
2013-10-05 17:11:09 -0500

    Created prompts for Expressions Personal

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,5 @@
-alert("This is in the js file");
+//Calculating how much one would need to spend on gas in a week
+var milesPerGallon = prompt("Your vehicle's fuel economy in miles per gallon is", "");
+var milesToWork = prompt("In miles, how far is work from home?", "");
+var daysOfWork = prompt("How may days of the week do you work?", "");
+var gasPrice = prompt("What is the current price of gas in your area? ", "");
