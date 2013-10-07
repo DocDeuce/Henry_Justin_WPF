047b16661d1d1bd4da76bdbb22b6a84c69f7255b
Justin Henry <doc2@fullsail.edu>
2013-10-07 10:21:17 -0500

    Completed formula for Expressions Wacky

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,12 @@
-alert("This is in the js file");
+//Drink away the ugly
+var rate = prompt("On a scale of 1-10, how ugly is the person you're looking at?", "");
+var date = prompt("How many weeks has it been since your last date?", "");
+var weight = prompt("How much do you weigh?", "");
+
+var bac = .06 + (rate * .02);
+var bacAlt = bac - ((date / 4) * .02);
+var drinks = ((.615 * weight) * (bacAlt +.015)) / 5.14;
+
+document.write(bac + "<BR>");
+document.write(bacAlt + "<BR>");
+document.write(drinks);
