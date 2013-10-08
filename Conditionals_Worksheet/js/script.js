cf47cbd7234eeb9c12143be3ea98132e7e72110b
Justin Henry <doc2@fullsail.edu>
2013-10-08 08:11:53 -0500

    Initial coding for "Last Chance for Gas"

--- Conditionals_Worksheet/js/script.js
+++ Conditionals_Worksheet/js/script.js
@@ -1,1 +1,19 @@
 //Justin Henry 10-8-2013 Conditionals Worksheet
+
+//Last Chance for Gas
+var mpg = 26;
+var gauge = 25;
+var capacity = 18.5;
+
+var gasLeft = capacity * (gauge / 100);
+var milesLeft = capacity * mpg;
+
+if (milesLeft > 200)
+{
+    consle.log("Yes, you can make it without stopping for gas!");
+}
+else
+{
+    consle.log("You only have " + gasLeft + "gallons of gas in your tank, better get gas now while you can!");
+}
+
