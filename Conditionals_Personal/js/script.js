b3e8c438b15972540a633b773bf2c2375fadc644
Justin Henry <doc2@fullsail.edu>
2013-10-17 21:48:19 -0500

    Initial Coding for Conditionals Personal

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,1 +1,18 @@
 //Justin Henry 10-16-2013 Conditionals Personal
+
+var milk = prompt("How many quarters of the milk carton remain?","");
+var veggies = prompt("How many meals worth of vegetables are left?", "");
+var meat = prompt("How many days worth of meet do you have?", "");
+
+var total = parseInt(milk) + parseInt(veggies) + parseInt(meat);
+
+if (total <= 4){
+    alert("Dude, go to the store NOW!");
+    console.log("Dude, go to the store NOW!");
+}else if ((total >= 5) && (total <=7)){
+    alert("You should be okay for a day or two.");
+    console.log("You should be okay for a day or two.");
+}else{
+    alert("All is well! Check back in a few days.");
+    console.log("All is well! Check back in a few days.");
+}
