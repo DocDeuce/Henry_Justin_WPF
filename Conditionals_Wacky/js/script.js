08ada3e020bba6a8d7d37c74ab71e32be5aad661
Justin Henry <doc2@fullsail.edu>
2013-10-17 22:59:33 -0500

    Some commenting on Conditionals Wacky

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -2,17 +2,17 @@
 
 alert("How much Woodchuck would a woodchuck chug if a woodchuck could chug Woodchuck?");
 
-var gender = prompt("Is it Mr. or Mrs. Woodchuck?","Mr. or Mrs.");
-var weight = prompt("How many pounds is this woodchuck packing?", "");
+var gender = prompt("Is it Mr. or Mrs. Woodchuck?","Mr. or Mrs."); //Prompt enter the woodchuck's gender'
+var weight = prompt("How many pounds is this woodchuck packing?", ""); //Prompt to enter the woodchuck's weight (they average 4-9 pounds)
 
-if ((isNaN(weight)) || (weight === "")){
-    alert("Please enter a numeric value for the weight of the woodchuck.");
+if ((isNaN(weight)) || (weight === "")){ //Validates weight entry by checking if weight is not a number or left blank
+    alert("Please enter a numeric value for the weight of the woodchuck."); //Alert if weight is not a number or left blank
 }
 if (!((gender === "Mr.") || (gender === "Mrs."))){
-    alert("Please enter 'Mr.' or 'Mrs.' to indicate the gender of the woodchuck.")
+    alert("Please enter 'Mr.' or 'Mrs.' to indicate the gender of the woodchuck."); //Alert
 }
 
-if ((weight >= 7) && (gender = "Mr.")){
+if ((weight >= 7) && (gender = "Mr.")){ //Checks for a woodchuck that is both male and 7 pounds or over
     alert("I hope you brought a six-pack.");
     console.log("I hope you brought a six-pack.");
 }else if (gender = "Mr."){
