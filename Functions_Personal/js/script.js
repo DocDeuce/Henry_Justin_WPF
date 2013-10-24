5d9b6cd3ca8f592ea7c2141ec89d223344c9f324
Justin Henry <doc2@fullsail.edu>
2013-10-24 00:48:18 -0500

    Initial coding for Functions Personal

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,1 +1,17 @@
 // Justin Henry 10-23-13 Functions Personal
+
+var driveTime = timeCalc(100, 80);
+
+function timeCalc(d, v){
+    var time = d / v;
+    return time;
+}
+console.log("It will take you about " + driveTime + " hours to make your trip.");
+
+if (driveTime <= 1.5){
+    console.log("Bring your favorite CD for the ride.");
+}else if (driveTime > 1.5 && driveTime < 3){
+    console.log("Bring a good audiobook for the ride.");
+}else{
+    console.log("Bring a friend for the ride.");
+}
