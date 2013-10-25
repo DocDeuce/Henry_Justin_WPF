2b5dce0d1b94f23cecd30c9c23676fab94b1640c
Justin Henry <doc2@fullsail.edu>
2013-10-24 22:38:24 -0500

    Commented on Functions Industry

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,16 +1,17 @@
 // Justin Henry 10-24-2013 Functions Industry
 
+//Considering how much it cost to make a certain number of merchandise units, at what price should each unit be sold in order to generate a desired profit?
 
-function priceCalc(){
-    var units;
-    var cost;
-    var profit;
-    var price;
-    units = 100;
-    cost = 200;
-    profit = 300;
-    price = (profit + cost) / units;
-    console.log("To make a profit of $" + profit + ", sell each unit for $" + price + ".");
+function priceCalc(){ //begins the function
+    var units; //declaration of units representing the number of merch units purchased to be resold
+    var cost; //declaration of cost representing the cost to produce the merch units
+    var profit; ///declaration of profits representing the desired outcome of sales
+    var price; //declaration of price to be determined by the equation to follow
+    units = 100; //100 merch units
+    cost = 200; //merch was purchased for $200
+    profit = 300; //A desired profit of $300
+    price = (profit + cost) / units; //Equation to solve for price
+    console.log("To make a profit of $" + profit + ", sell each unit for $" + price + "."); //Tells the user how much each merch unit should be sold to reach the desired profit
 }
 
-priceCalc()
+priceCalc() //invocation of the function
