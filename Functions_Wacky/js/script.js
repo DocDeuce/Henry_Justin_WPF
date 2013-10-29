4c3b594bf4e164f6021baae1378c20a559569799
Justin Henry <doc2@fullsail.edu>
2013-10-28 23:55:21 -0500

    Code ideas for Functions Wacky

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,3 +1,19 @@
 // Justin Henry 10-24-2013 Functions Wacky
 
-alert("I still have trouble coming up with wacky stuff");
+//How many Asian weaver ants would it take to carry a certain weight?
+
+var weight = prompt("Enter the weight to be carried in kilograms.", "");
+if ((isNaN(weight)) || (weight === "")){
+
+}
+
+var carry;
+var method = prompt("Will the weight be carried across the floor or the ceiling? Type 'floor' or 'ceiling.'", "");
+if (method === "ceiling"){
+    carry = 5;
+}else if (method === "floor"){
+    carry = 2;
+}else{
+
+}
+
