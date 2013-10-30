fed8d41bd3ce38002699898f8a7baedd7c863128
Justin Henry <doc2@fullsail.edu>
2013-10-29 22:03:30 -0500

    Updated code for Functions Wacky
    Included a loop and equation for solving final problem

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -2,18 +2,30 @@
 
 //How many Asian weaver ants would it take to carry a certain weight?
 
-var weight = prompt("Enter the weight to be carried in kilograms.", "");
+/*var weight = prompt("Enter the weight to be carried in kilograms.", "");
 if ((isNaN(weight)) || (weight === "")){
 
-}
+}*/
+
+var weight;
+do{
+    weight = prompt("Enter the weight to be carried in kilograms.", "");
+}while((isNaN(weight)) || (weight === ""))
+
+console.log(weight);
+
 
 var carry;
-var method = prompt("Will the weight be carried across the floor or the ceiling? Type 'floor' or 'ceiling.'", "");
+var method;
+method = prompt("Will the weight be carried across the floor or the ceiling? Type 'floor' or 'ceiling.'", "");
 if (method === "ceiling"){
     carry = 5;
 }else if (method === "floor"){
     carry = 2;
 }else{
-
+    method = prompt("Type 'floor' or 'ceiling.'", "");
 }
 
+/*
+var ants = (weight * 10000) / carry;
+*/
